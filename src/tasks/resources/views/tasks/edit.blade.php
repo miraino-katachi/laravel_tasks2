9706d083152b003875137419e648aa60a2d519b8
坂上　正 <t_sakagami@miraino-katachih2.co.jp>
2021-04-11 20:44:00 +0900

    修正画面のold()ヘルパ関数の使い方を修正。

--- src/tasks/resources/views/tasks/edit.blade.php
+++ src/tasks/resources/views/tasks/edit.blade.php
@@ -15,7 +15,7 @@
     <div class="form-group">
         <label for="title">タイトル</label>
         <input type="text" name="title" id="title" class="form-control @error('title') is-invalid @enderror "
-            value="{{ old('title') ? old('title') : $task->title  }}">
+            value="{{ old('title', $task->title) }}">
         @error('title')
         <div id="validateTitle" class="invalid-feedback">
             {{ $message }}
@@ -26,7 +26,7 @@
         <label for="expiration_date">期限日</label>
         <input type="date" name="expiration_date" id="expiration_date"
             class="form-control @error('expiration_date') is-invalid @enderror "
-            value="{{ old('expiration_date') ? old('expiration_date') : $task->expiration_date }}">
+            value="{{ old('expiration_date',$task->expiration_date) }}">
         @error('expiration_date')
         <div id="validateExpirationDate" class="invalid-feedback">
             {{ $message }}
@@ -37,7 +37,7 @@
         <label for="completion_date">完了日</label>
         <input type="date" name="completion_date" id="completion_date"
             class="form-control @error('completion_date') is-invalid @enderror "
-            value="{{ old('completion_date') ? old('completion_date') : $task->completion_date }}">
+            value="{{ old('completion_date', $task->completion_date) }}">
         @error('completion_date')
         <div id="validateCompletionDate" class="invalid-feedback">
             {{ $message }}
@@ -47,7 +47,7 @@
     <div class="form-group">
         <label for="description">説明</label>
         <textarea name="description" id="descriptin" rows="5"
-            class="form-control @error('description') is-invalid @enderror ">{{ old('description') ? old('description') : $task->description }}</textarea>
+            class="form-control @error('description') is-invalid @enderror ">{{ old('description', $task->description) }}</textarea>
         @error('description')
         <div id="validateDescription" class="invalid-feedback">
             {{ $message }}
